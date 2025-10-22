608273e26eeb5b6c5cf4e1aae4c24706453729db
kemillybkf <kemillybkf@gmail.com>
2025-10-22 17:18:26 -0300

    Update projetin.php

--- projetin.php
+++ projetin.php
@@ -1,29 +1,18 @@
 <?php
-/*$nome = $_POST["nome"];
-$sobrenome = $_POST["sobrenome"];
-$email = $_POST["email"];
-$senha = $_POST["senha"];*/
-
+include "config.php"
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    if (isset($_POST["nome"],$_POST["sobrenome"], $_POST["email"], $_POST["senha"])){
-        $nome = htmlspecialchars($_POST["nome"]);
-        $sobrenome = htmlspecialchars($_POST["sobrenome"]);
-        $email = htmlspecialchars($_POST["email"]);
-        $senha = htmlspecialchars($_POST["senha"]);
-        if (empty($nome)|| empty($sobrenome)|| empty($email) || empty($senha)){
-            echo "Todos os campos são obrigatórios!";
+        $nome = htmlspecialchars($_POST['nome']);
+        $sobrenome = htmlspecialchars($_POST['sobrenome']);
+        $data_nascimento = $_POST['data_nascimento'];
+        $email = htmlspecialchars($_POST['email']);
+        $senha = htmlspecialchars($_POST['senha']);
+        $sql = "INSERT INTO cadastro (nome, sobrenome, data_nascimento, email, senha)
+                VALUE ('$nome', '$sobrenome', '$data_nascimento', '$email', '$senha')";
+        if (mysqli_query($conn, $sql)) {
+            echo "Cadastro realizado com sucesso!";
+        } else {
+            echo "Erro ao cadastrar: " . mysqli_error($conn);
         }
-        elseif (strlen($senha ) < 6){
-            echo "A senha deve ter pelo menos 6 caracteres!";
-        }
-        else{
-            echo "<h3>Cadastro realizado com sucesso!</h3>";
-            echo "Nome: $nome<br>";
-            echo "E-mail: $email<br>";
-            echo "Senha: $senha<br>";
-        }
-    }else{
-        echo "Erro: Campos não encontrados no formulário.";
-    }
+        mysqli_close($conn);
 }
 ?>
