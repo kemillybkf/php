2e436dcc5728b7e26d01516045fddc094f1eeba6
kemillybkf <kemillybkf@gmail.com>
2025-10-28 17:10:40 -0300

    Update logout2.php

--- logout2.php
+++ logout2.php
@@ -1,7 +1,9 @@
+
 <?php
     session_start();
     session_unset();
     session_destroy();
+    setcookie('lembrar-senha','', time() - 3600,  "/");
     header("Location: loginket.html");
     exit;
 ?>
