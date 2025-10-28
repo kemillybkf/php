71962bbb93fd25c89a7d3455cd9954fff7d7f907
kemillybkf <kemillybkf@gmail.com>
2025-10-28 17:07:03 -0300

    Update config2.php

--- config2.php
+++ config2.php
@@ -1,11 +1,18 @@
 
+
 <?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $host = "sql100.infinityfree.com";
 $usuario = "if0_40230926";
 $senha = "345838311";
 $banco =  "if0_40230926_ketfy";
 $conn =  new mysqli($host, $usuario, $senha, $banco);
-if ($conn->connect_error) {
+if ($conn->connect_error) 
     die("erro na conexão". $conn->connect_error);
+try{
+    $conn = new mysqli($host, $usuario, $senha, $banco);
+    $conn->set_charset("utf8mb4");
+}catch(mysqli_sql_exception $e){
+    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
 }
 ?>
