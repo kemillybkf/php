84e6f6914b9315b26ef2618eca93cfc3fe85ff0e
kemillybkf <kemillybkf@gmail.com>
2025-10-28 17:08:27 -0300

    Update ketfor.php

--- ketfor.php
+++ ketfor.php
@@ -1,40 +1,31 @@
+
 <?php
-/*
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$senha = $_POST["senha"];
-
-echo "Olá $nome, seu e-email é $email";
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    if (isset($_POST["nome"], $_POST["email"], $_POST["senha"])){
-        $nome = htmlspecialchars($_POST["nome"]);
-        $email = htmlspecialchars($_POST["email"]);
-        $senha = htmlspecialchars($_POST["senha"]);
-        if (empty($nome) || empty($email) || empty($senha)){
-            echo "Todos os campos são obrigatórios!";
-        }
-        elseif (strlen($senha ) < 6){
-            echo "A senha deve ter pelo menos 6 caracteres!";
-        }
-        else{
-            echo "<h3>Cadastro realizado com sucesso!</h3>";
-            echo "Nome: $nome<br>";
-            echo "E-mail: $email<br>";
-            echo "Senha: $senha<br>";
-        }
-    }else{
-        echo "Erro: Campos não encontrados no formulário.";
-    }
-}*/
-if (isset($_POST["linguagens"])){
-    $linguagens = $_POST["linguagens"];
-    echo "Você escolheu:<br>";
-    foreach ($linguagens as $ling){
-        echo "-$ling<br>";
-    }
-} else {
-    echo "Nenhuma linguagem foi selecionada.";
+include"config.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nome = htmlspecialchars($_POST['nome']);
+        $sobrenome = htmlspecialchars($_POST['sobrenome']);
+        $data_nascimento = $_POST['data_nascimento'];
+        $email = htmlspecialchars($_POST['email']);
+        $senha = htmlspecialchars($_POST['senha']);
+        try{
+            $valida = "SELECTT 1 FROM cadastro WHERE email = '$email'";
+            $result = mysqli_query($conn, $valida);
+            if(mysqli_num_rows($result) > 0){
+                die("Erro: Email já cadastrado.");
+            }else{
+        $hash_senha = password_hash($senha, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO cadastro (nome, sobrenome, data_nascimento, email, senha)
+                VALUES ('$nome', '$sobrenome', '$data_nascimento', '$email', '$senha', '$hash_senha')";
+     (mysqli_query($conn, $sql));
+            echo 'Cadastro realizado com sucesso! <a href= "loginket.html">Login</a>';
+            }
+        } catch (mysqli_sql_exception $e) {
+            echo "Erro ao processar o cadastro. ".$e->getMessage();
+        }finally{
+        mysqli_close($conn);
 }
+
+    }else{
+        echo"Campos não encontrados no formulário!";
+    }
 ?>
