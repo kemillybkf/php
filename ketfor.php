3883fb352fb7c73c85ac7e386a6275a32554bb7b
kemillybkf <kemillybkf@gmail.com>
2025-10-30 17:07:36 -0300

    Update ketfor.php

--- ketfor.php
+++ ketfor.php
@@ -1,4 +1,3 @@
-
 <?php
 include"config.php";
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -7,25 +6,30 @@ include"config.php";
         $data_nascimento = $_POST['data_nascimento'];
         $email = htmlspecialchars($_POST['email']);
         $senha = htmlspecialchars($_POST['senha']);
-        try{
-            $valida = "SELECTT 1 FROM cadastro WHERE email = '$email'";
-            $result = mysqli_query($conn, $valida);
-            if(mysqli_num_rows($result) > 0){
-                die("Erro: Email já cadastrado.");
-            }else{
-        $hash_senha = password_hash($senha, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO cadastro (nome, sobrenome, data_nascimento, email, senha)
-                VALUES ('$nome', '$sobrenome', '$data_nascimento', '$email', '$senha', '$hash_senha')";
-     (mysqli_query($conn, $sql));
-            echo 'Cadastro realizado com sucesso! <a href= "loginket.html">Login</a>';
-            }
-        } catch (mysqli_sql_exception $e) {
-            echo "Erro ao processar o cadastro. ".$e->getMessage();
-        }finally{
-        mysqli_close($conn);
-}
-
-    }else{
-        echo"Campos não encontrados no formulário!";
     }
+        try{
+            $valida = $conn->prepare("SELECT 1 FROM cadastro WHERE email=?");
+            $valida->bind_param('s', $email);
+            $valida->execute();
+            $valida->get_result();
+            if($valida->num_rows > 0){
+                echo "E-mail já cadastrado.";
+                $valida-> close();
+                $conn-> close();
+                exit;
+            }
+        $valida->close();
+        $hash_senha = password_hash($senha, PASSWORD_DEFAULT);
+        $sql = $conn->prepare("INSERT INTO cadastro (nome, sobrenome, data_nascimento, email, senha)
+                VALUES (?, ?, ?, ?, ?)");
+                $sql-> bind_param('ssssss', $nome, $sobrenome, $data_nascimento, $email, $hash_senha);
+                $sql-> execute();
+                $sql-> close();
+        
+            echo "Cadastro realizado com sucesso! <a href='loginket.html'>Login";
+        } catch (mysqli_sql_exception $e) {
+            echo "Erro ao cadastrar: " .$e->getMessage();
+        }
+        mysqli_close($conn);
+     
 ?>
