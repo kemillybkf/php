d72fde6f23df989253d9115b0c96b10d75e46361
kemillybkf <kemillybkf@gmail.com>
2025-10-30 17:08:37 -0300

    Update ketp2.php

--- ketp2.php
+++ ketp2.php
@@ -5,12 +5,19 @@
     <title>Painel</title>
 </head>
 <body>
-    <h2>Bem-vindo,
+    <h2>Olá
     <?php
         session_start();
-        echo htmlspecialchars($SESSION['usuario_nome']);
+        if (!isset($_SESSION['usuario'])){
+            header("Location: login.html");
+            exit;
+        }else{
+        echo htmlspecialchars($_SESSION['usuario_nome']);
+        }
     ?>
 </h2>
 <p><a href= "loginket.php">Sair</a></p>
+<br>
+<p><a href="alterket.html">Alterar Senha</a></p>
 </body>
 </html>
